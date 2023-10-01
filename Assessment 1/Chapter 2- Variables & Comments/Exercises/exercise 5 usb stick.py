85b8177d7158a81f7071be5bc67d81242d332c3b
Asad59917 <144930871+Asad59917@users.noreply.github.com>
2023-10-01 10:34:31 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 2- Variables & Comments/Exercises/exercise 5 usb stick.py
@@ -0,0 +1,8 @@
+usb_cost = 6
+Budget = 50
+
+Usb_she_can_buy=Budget // usb_cost
+money_left = Budget % usb_cost
+
+print ("The girl can buy", Usb_she_can_buy,"usb sticks")
+print ("The girl will be left with",money_left)
