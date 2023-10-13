69405692ec7877e5f4be8f05cd949a4a3d9cd274
Asad59917 <144930871+Asad59917@users.noreply.github.com>
2023-10-13 16:49:08 +0400

    Update exercise 5 usb stick.py

--- Assessment 1/Chapter 2- Variables & Comments/Exercises/exercise 5 usb stick.py
+++ Assessment 1/Chapter 2- Variables & Comments/Exercises/exercise 5 usb stick.py
@@ -1,8 +1,8 @@
-usb_cost = 6
+usb_cost = 6    #first we will assign variable the value 
 Budget = 50
 
-Usb_she_can_buy=Budget // usb_cost
-money_left = Budget % usb_cost
+Usb_she_can_buy=Budget // usb_cost         # now we will use // because we only need number we don't need desimal 
+money_left = Budget % usb_cost             # at at the end wwe will show the money left with her 
 
-print ("The girl can buy", Usb_she_can_buy,"usb sticks")
+print ("The girl can buy", Usb_she_can_buy,"usb sticks")   # now we will print both 
 print ("The girl will be left with",money_left)
