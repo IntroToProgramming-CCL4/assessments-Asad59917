4c8693bea10a8b72151f45848b7be11ccb237636
Asad59917 <144930871+Asad59917@users.noreply.github.com>
2023-11-09 11:18:05 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 2- Variables & Comments/Exercises/Chap 2 ex 3updated.py
@@ -0,0 +1,16 @@
+# we will write my name wih whitespace around it 
+My_name = "\t \n   John Doe \n \t"
+
+# Now we will print the my name with whitespace 
+print("My name with whitespace:")
+print(My_name)
+
+# Now we will apply stippring functions to print my name 
+print("\nUsing lstrip():")
+print(My_name.lstrip())
+
+print("\nUsing rstrip():")
+print(My_name.rstrip())
+
+print("\nUsing strip():")
+print(My_name.strip())
